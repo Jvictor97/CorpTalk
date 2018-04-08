c563dbc0ea7eed3efa2f8c930e54cc456f62f117
Souza <d0128705@bemisco.net>
2018-04-08 01:26:12 -0300

    Fixed 'online users' only show actual user

--- server.js
+++ server.js
@@ -17,10 +17,11 @@ io.sockets.on('connection', function(socket){
 
 	// Disconnect
 	socket.on('disconnect', function(data){
-		users.splice(users.indexOf(socket.username),1);
-		updateUsernames();
 		connections.splice(connections.indexOf(socket), 1);
 		console.log('Disconnected, %s sockets connected', connections.length);
+		if(!socket.username) return;
+		users.splice(users.indexOf(socket.username),1);
+		updateUsernames();
 	});
 
 	// Send Message
